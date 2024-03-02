91aa0e0a5fff8273f0f4afe6573ca74d292378dd
Anish Dubey <anishdubey0712@gmail.com>
2024-03-02 17:35:22 +0530

    Made changes is rating and fixed it to star rating

--- routes/review.js
+++ routes/review.js
@@ -6,30 +6,14 @@ const ExpressError = require("../utils/ExpressError.js");
 const {validateReview, isLoggedIn, isReviewAuthor} =  require("../middleware.js")
 const Review = require("../models/review");
 const Listing = require("../models/listing.js");
+const reviewController = require("../controllers/reviews.js")
 
 
 
 //Reviews Route(Post route)
-router.post("/",isLoggedIn,validateReview,wrapAsync(async(req,res)=>{
-    //Here both are used validateReview for review err handling and wrapAsync for basic err handling
-    let listing = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review);// we take review from req body and add it as new review in schema model
-    newReview.author = req.user._id;//If user is logged in then he will be our author and then we'll push author in review 
-    listing.reviews.push(newReview); // ad we will push that into our reviews model
-    await newReview.save();//we will save it to our db
-    await listing.save(); 
-    req.flash("success","New Review Added!")
-    res.redirect(`/Listings/${listing._id}`);
-}));
+router.post("/",isLoggedIn,validateReview,wrapAsync(reviewController.createReview));
 
 //Delete Reviews Route
-router.delete("/:reviewId",isLoggedIn,isReviewAuthor,wrapAsync(async(req,res)=>{
-   let{id , reviewId} = req.params;
-   await Listing.findByIdAndUpdate(id,{$pull: {reviews : reviewId}});
-   //we've used pull operator of Mongodb.So, we've passed our id then from reviews array any id get matched with our passed id we'll pull it and remove it.
-   await Review.findByIdAndDelete(reviewId);//by pulling our reviewId here we'll delete it
-   req.flash("success","Review Deleted!")
-   res.redirect(`/Listings/${id}`);
-}));
+router.delete("/:reviewId",isLoggedIn,isReviewAuthor,wrapAsync(reviewController.deleteReview));
 
 module.exports=router;
--- routes/user.js
+++ routes/user.js
@@ -4,60 +4,20 @@ const router = express.Router();
 const User= require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
 const passport =  require("passport");
-const {saveRedirectUrl}=require("../middleware.js")
+const {saveRedirectUrl}=require("../middleware.js");
+const userController = require("../controllers/users.js");
 
 //SignUp Route
-router.get("/signup",(req,res)=>{
-    res.render("users/signup.ejs")
-});
-
-router.post("/signup",wrapAsync (async (req,res)=>{
-    try{
-     let {username,email,password}=req.body;//We'll get all details from user input body
-const newUser =  new User({email,username}); // we'll make new user in db
-const registerUser = await User.register(newUser,password); //By register method we'll reg it to db
-//req.login is passport method which automatically login user after signup 
-req.login(registerUser,(err)=>{
-    if(err){
-        next(err);
-    }
-    req.flash("success","Welcome to WanderLust");
-res.redirect(res.locals.redirectUrl);
-})
-}
-catch(err){
-req.flash("error",err.message);// we'll catch err and display err to user
-res.redirect("/signup"); // and redirect to same page
-}
-}));
-
+router.get("/signup",userController.renderSignUp);
+//Post signup Route
+router.post("/signup",wrapAsync (userController.signUP));
 //Login Route
-router.get("/login",(req,res)=>{
-    res.render("users/login.ejs");
-    
-});
-
-//Post req route will check user details in db and match it 
-//all this authentication work will do our passport middleware
-//in this authenticate middleware 1st we pass our Stratergy so we've used local stratergy 
-//And if login fails we'll redirect to login page again
-router.post("/login",saveRedirectUrl,passport.authenticate("local",{failureRedirect: '/login',failureFlash: true}),
-async (req,res)=>{
-    const clientName = req.user.username; // By this we'll get username from req and then we can pass it with flash
-
-    req.flash("success",`Welcome Back ${clientName}`);
-    let redirectUrl = res.locals.redirectUrl || "/Listings";//If redirectUrl is saved url path then we'll use it otherwise if redirectUrl is undefined then we'll redirect to listings page
-res.redirect(redirectUrl);//req.session
-});
-router.get("/logout",(req,res)=>{
-    req.logOut((err)=>{
-        if(err){
-            return next(err);
-    }
-    req.flash("success","You are successfully logged out!")
-    res.redirect("/Listings")
-})
-})
-
-
+router.get("/login",userController.renderLogin);
+//Login Post route
+router.post("/login",saveRedirectUrl,
+passport.authenticate("local",{failureRedirect: '/login',failureFlash: true}),
+userController.Login);
+//Logout
+router.get("/logout",userController.Logout)
+//Router
 module.exports=router;
