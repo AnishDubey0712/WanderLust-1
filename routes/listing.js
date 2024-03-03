65cab4017d456519f8068eb69a2cbdf8e99d4511
Anish Dubey <anishdubey0712@gmail.com>
2024-03-03 15:45:51 +0530

    Changes fixed

--- routes/listing.js
+++ routes/listing.js
@@ -6,7 +6,7 @@ const Listing = require("../models/listing.js");
 const {isLoggedIn, isOwner,validateListing}=require("../middleware.js");//required to pass as middleware in func.
 const listingController = require("../controllers/listings.js");
 const multer = require("multer"); //Multer is middleware npm package used to handle multipart/form-data
-const upload = multer({dest: '/uploads'});//Multer will get data(file) & automatically make upload folder & save files
+const upload = multer({dest: 'uploads/'});//Multer will get data(file) & automatically make upload folder & save files
 //But later we'll use 3rd party cloud service to upload these files
 //Index route
 router.get("/",wrapAsync(listingController.index));//this index is defined in controllers/listings.js
